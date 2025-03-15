e7286ec1cdcbbbe58966a6395af16f357f713ad1
Tor Børve Rasmussen <rasmustor18@gmail.com>
2025-03-15 23:50:54 +0100

    using my fork and updated build.rs

--- build.rs
+++ build.rs
@@ -1,30 +1,20 @@
 use make_cmd::make;
-use std::{env, path::PathBuf};
+use std::{
+    env,
+    path::{Path, PathBuf},
+};
 
 fn main() {
     let slicot_dir = PathBuf::from("SLICOT-Reference/");
-
-    let make_file = if cfg!(target_os = "windows") {
-        "makefile"
-    } else {
-        "makefile_Unix"
-    };
-
-    let mut make_args: Vec<String> = vec![
-        "lib".to_string(),
-        "-f".to_string(),
-        make_file.to_string(),
+    let slicot_build_dir = Path::new(&env::var("OUT_DIR").unwrap())
+        .join("slicot_build")
+        .to_str()
+        .unwrap()
+        .to_string();
+    let make_args: Vec<String> = vec![
         "-j8".to_string(),
+        format!("BUILD_DIR={}", slicot_build_dir).to_string(),
     ];
-    if cfg!(target_os = "windows") {
-        make_args.push("HOME=.".to_string());
-    } else {
-        make_args.push("SLICOTLIB=../libslicot.a".to_string());
-        make_args.push("LPKAUXLIB=../liblpkaux.a".to_string());
-        if let Ok(fc) = env::var("FC") {
-            make_args.push(format!("FORTRAN={}", fc).to_string());
-        }
-    }
 
     let status = make()
         .current_dir(&slicot_dir)
@@ -33,10 +23,16 @@ fn main() {
         .expect("Failed to build make");
 
     if !status.success() {
+        if cfg!(target_os = "windows") {
+            println!(
+                "cargo:warning=Control Systems Torbox has not been tested on windows. You are likely to have issues with compiling SLICOT."
+            )
+        }
+        println!("cargo:warning=Failed to compile SLICOT library");
         panic!("Failed to build SLICOT")
     }
 
-    println!("cargo:rustc-link-search={}", slicot_dir.to_str().unwrap());
+    println!("cargo:rustc-link-search={}", slicot_build_dir);
     println!("cargo:rustc-link-lib=static=slicot");
     println!("cargo:rustc-link-lib=static=lpkaux");
     println!("cargo:rustc-link-lib=gfortran");
