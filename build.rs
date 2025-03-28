5393ef40d0a5a6e3909855a5f99908e8774d0a5f
Tor Børve Rasmussen <rasmustor18@gmail.com>
2025-03-28 21:50:42 +0100

    fixed problem with netlibsrc using cuBLAS not BLAS

--- build.rs
+++ build.rs
@@ -36,7 +36,4 @@ fn main() {
     println!("cargo:rustc-link-lib=static=slicot");
     println!("cargo:rustc-link-lib=static=lpkaux");
     println!("cargo:rustc-link-lib=gfortran");
-    println!("cargo:rustc-link-lib=blas");
-    println!("cargo:rustc-link-lib=lapack");
-    println!("cargo:rustc-link-lib=m");
 }
